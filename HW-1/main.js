760c575fa9a2b911b89b3cd5e9a8752882cfee97
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-05-01 22:09:07 +0300

    HW-1

---
+++ HW-1/main.js
@@ -0,0 +1,55 @@
+// task 1
+let hello = "hello";
+let owu = "owu";
+let com = "com";
+let ua = "ua";
+
+let number = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 = 3.14;
+let number6 = 2.7;
+let number7 = 16;
+
+let boolean = true;
+let boolean2 = false;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(number);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+console.log(number6);
+console.log(number7);
+console.log(boolean);
+console.log(boolean2);
+
+//task 2
+let firstName = "Marichka";
+let middleName = "Vitalyivna";
+let lastName = "Tanechnyk";
+let person = firstName + " " + middleName + " " + lastName;
+console.log(person);
+
+//task 3
+let a = 100;
+console.log(typeof a);
+let b = "100";
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+//additional task
+let firstNamePrompt = prompt("What is your first name?");
+console.log(firstNamePrompt);
+let middleNamePrompt = prompt("What is your middle name?");
+console.log(middleNamePrompt);
+let lastNamePrompt = prompt("What is your last name?");
+console.log(lastNamePrompt);
+let age = prompt("What is your age?");
+console.log(age);
